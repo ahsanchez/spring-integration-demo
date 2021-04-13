7e9a7176f074736d6f503af469491c91bc657248
ana.hinojosa <ana.hinojosa@ezentis.com>
2021-04-13 17:14:49 +0200

    section 7 async gateways

--- src/main/java/com/oreilly/demo/PrintService.java
+++ src/main/java/com/oreilly/demo/PrintService.java
@@ -1,12 +1,13 @@
 package com.oreilly.demo;
 
 import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PrintService {
 
     public void print(Message<?> message) {
         System.out.println(message.getPayload());
         message.getHeaders().forEach((key, value) -> System.out.println(key + ": " + value));
     }
-
 }
--- src/main/java/com/oreilly/demo/SpringIntegrationDemoApplication.java
+++ src/main/java/com/oreilly/demo/SpringIntegrationDemoApplication.java
@@ -7,10 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.integration.support.MessageBuilder;
-import org.springframework.messaging.Message;
-
-import java.util.concurrent.ExecutionException;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @SpringBootApplication
 @Configuration
@@ -18,18 +16,33 @@ import java.util.concurrent.ExecutionException;
 public class SpringIntegrationDemoApplication implements ApplicationRunner {
 
     @Autowired
-    private PrinterGateway gateway;
+    private EnhancedPrinterGateway gateway;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringIntegrationDemoApplication.class, args);
     }
 
     @Override
-    public void run(ApplicationArguments arg0) throws InterruptedException, ExecutionException {
+    public void run(ApplicationArguments arg0) {
         Person[] payloads = {new Person("Ana", "Hinojosa"), new Person("Pilar", "Molina"), new Person("Jose Carlos", "Mendoza")};
         for (Person payload : payloads) {
-            Message<?> message = MessageBuilder.withPayload(payload).setHeader("privateKey", "12345").build();
-            this.gateway.print(message);
+            //String uppercase = this.gateway.uppercase(payload);
+            //System.out.println(uppercase);
+            //this.gateway.print(payload);
+            System.out.println("Invoking the gateway method");
+            ListenableFuture<String> future = this.gateway.uppercase(payload);
+            future.addCallback(new ListenableFutureCallback<String>() {
+                @Override
+                public void onFailure(Throwable ex) {
+                    System.out.println("Invoking the failure callback");
+                }
+
+                @Override
+                public void onSuccess(String result) {
+                    System.out.println("Invoking the success callback");
+                    System.out.println(result);
+                }
+            });
         }
 
     }
