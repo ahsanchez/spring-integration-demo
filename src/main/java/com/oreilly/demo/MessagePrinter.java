ae2d252569b74e94fad71c8e5091e59037332988
ana.hinojosa <ana.hinojosa@ezentis.com>
2021-04-16 11:23:58 +0200

    section 10 jms in-outbound gateway

--- src/main/java/com/oreilly/demo/MessagePrinter.java
+++ src/main/java/com/oreilly/demo/MessagePrinter.java
@@ -7,8 +7,11 @@ import org.springframework.stereotype.Component;
 public class MessagePrinter {
 
     public String print(Message<?> message) {
-        System.out.println("Print the message:");
+        return "From the inbound gateway: 1 message worked";
+    }
+
+    public void printConsole(Message<?> message) {
+        System.out.println("Message was passed to printConsole from the outboundGateway");
         System.out.println(message);
-        return "1 message worked";
     }
 }
